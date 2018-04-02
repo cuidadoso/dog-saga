f4fa91116ec876489cb9153ea3d94ec0b80e82b3
Alexander Pyreev <apyreev@at-consulting.ru>
2018-04-02 13:40:17 +0300

    Connect redux-store and saga to App

--- src/redux.js
+++ src/redux.js
@@ -14,13 +14,10 @@ export function reducer(state = initialState, action) {
     switch (action.type) {
         case API_CALL_REQUEST:
             return { ...state, fetching: true, error: null };
-            break;
         case API_CALL_SUCCESS:
             return { ...state, fetching: false, dog: action.dog };
-            break;
         case API_CALL_FAILURE:
             return { ...state, fetching: false, dog: null, error: action.error };
-            break;
         default:
             return state;
     }
